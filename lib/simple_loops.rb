e83b005d3ff29537ca1fc37044aa4d2822618d52
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 16:58:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,12 @@ def loop_message_n_times(message, number)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
