ec0bf45a2dbfc862bac6a8cdc869b8f5f6548448
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 17:01:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,7 +29,7 @@ end
 def return_string_array(array)
   counter = 0
   while count < array.length do
-    array[count] = array[count].to_s
+    array[counter] = array[counter].to_s
     counter += 1
   end
   array
